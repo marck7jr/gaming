a18807313ae61eb567ff3bbd45111de6eb5b0526
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2021-01-30 03:00:33 -0400

    Add GetMessageParser generic method

--- src/Marck7JR.Gaming.Web.IGDB/Google/Protobuf/MessageParserHelper.cs
+++ src/Marck7JR.Gaming.Web.IGDB/Google/Protobuf/MessageParserHelper.cs
@@ -1,12 +1,12 @@
-﻿using Google.Protobuf;
-using System;
+﻿using System;
 using System.Reflection;
 
-namespace Marck7JR.Gaming.Web.IGDB.Google.Protobuf
+namespace Google.Protobuf
 {
     public static class MessageParserHelper
     {
         public static MessageParser GetMessageParser(Type type) => (MessageParser)type.GetProperty("Parser", BindingFlags.Public | BindingFlags.Static).GetValue(null, null);
+        public static MessageParser GetMessageParser<T>() where T : IMessage => GetMessageParser(typeof(T));
         public static MessageParser GetMessageParser<T>(this T _) where T : IMessage => GetMessageParser(typeof(T));
     }
 }
